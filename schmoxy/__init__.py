7269d0ca9ec79541143b37eea559db518f7373d4
Ulas Tuerkmen <ulas.tuerkmen@retresco.de>
2013-10-07 14:08:11 +0200

    Added mapping of img src attributes, fetching and serving them.

--- schmoxy/__init__.py
+++ schmoxy/__init__.py
@@ -5,44 +5,80 @@ from flask import (Flask,
                    render_template,
                    request,
                    redirect,
-                   url_for)
+                   make_response)
 import requests
 from BeautifulSoup import BeautifulSoup
 
 app = Flask('adana')
 app.config.from_pyfile('configs/config.cfg')
 
+class BiDict(object):
+    def __init__(self):
+        self.one = dict()
+        self.two = dict()
+
+    def __getitem__(self, key):
+        try:
+            return self.one[key]
+        except KeyError:
+            return self.two[key]
+
+    def __setitem__(self, key, value):
+        print "MAPPED ", key, " AND ", value
+        self.one[key] = value
+        self.two[value] = key
+
+urls = BiDict()
+parsed_origin = urlparse(app.config['PROXY_ORIGIN'])
+
+def fetch_index():
+    try:
+        with open(os.path.join(app.config['CACHE_PATH'],
+                               'index'), 'r') as cached:
+            page_content = cached.read()
+    except IOError:
+        print "downloading page"
+        page = requests.get(app.config['PROXY_ORIGIN'])
+        print "page downloaded"
+        page_content = replace_references(page.text, app.config['PROXY_ORIGIN'])
+        with open(os.path.join(app.config['CACHE_PATH'],
+                               'index'), 'w') as cached:
+            cached.write(page_content.encode('utf-8'))
+    return page_content
+
+
 def replace_references(page_text, source_url):
+    global urls
     soup = BeautifulSoup(page_text)
     for img in soup.findAll('img'):
         src = urlparse(img['src'])
-        import pdb;pdb.set_trace()
         if not src.netloc:
+            #we want the absolut url so that it can be downloaded later
             src = urlparse(urljoin(source_url, img['src']))
-        img['src'] = ParseResult(src.scheme,
-                                 app.config['SERVER_NAME'],
-                                 src.path, src.params, src.query, src.fragment).geturl()
-        #img['src'] = 'http://shift.retresco.de/static/img/shift_logo_20x115.png'
+        try:
+            new_src = urls[src.geturl()]
+        except KeyError:
+            new_src = ParseResult('http',
+                                  app.config['SERVER_NAME'],
+                                  src.path, src.params, src.query, src.fragment).geturl()
+            urls[src.geturl()] = new_src
+        img['src'] = new_src
     return unicode(soup)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     if not path:
-        try:
-            with open(os.path.join(app.config['CACHE_PATH'],
-                                   'index'), 'r') as cached:
-                page_content = cached.read()
-        except IOError:
-            page = requests.get(app.config['PROXY_ORIGIN'])
-            page_content = replace_references(page.text, app.config['PROXY_ORIGIN'])
-            with open(os.path.join(app.config['CACHE_PATH'],
-                                   'index'), 'w') as cached:
-                cached.write(page_content.encode('utf-8'))
-        return page_content
-
-    print "NEED ", path
-    return requests.get(urljoin(app.config['PROXY_ORIGIN'], path)).text
+        return fetch_index()
+    real_path = "http://%s/%s" % (app.config['SERVER_NAME'], path)
+    resp = requests.get(urljoin(app.config['PROXY_ORIGIN'], urls[real_path]))
+    if resp.status_code != 200:
+        return ''
+    response = make_response(resp.content)
+    response.headers['content-length'] = response.headers['content-length']
+    response.headers['content-type'] = response.headers['content-type']
+    return response
 
 
 if __name__ == '__main__':
