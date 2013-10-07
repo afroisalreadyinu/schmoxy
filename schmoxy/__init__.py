23e2603e1f1b4cd4105cd4daea069d04b8c62531
Ulas Tuerkmen <ulas.tuerkmen@retresco.de>
2013-10-07 11:45:02 +0200

    added config values, requests dependency, downloading index

--- schmoxy/__init__.py
+++ schmoxy/__init__.py
@@ -1,15 +1,22 @@
+import os
+
 from flask import (Flask,
                    render_template,
                    request,
                    redirect,
                    url_for)
+import requests
 
 app = Flask('adana')
 app.config.from_pyfile('configs/config.cfg')
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if not path:
+        #app.config['CACHE_PATH']
+        page = requests.get(app.config['PROXY_ORIGIN'])
+        return page.text
 
-def run():
+if __name__ == '__main__':
     app.run()
